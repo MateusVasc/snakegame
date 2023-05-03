3f7c576f7928cc27793471249e5e04790bc7b8c1
MateusVasc <109766889+MateusVasc@users.noreply.github.com>
2023-05-02 22:41:15 -0300

    início do projeto.

---
+++ GamePanel.java
@@ -0,0 +1,60 @@
+package snakegame;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.util.Random;
+
+public class GamePanel extends JPanel  implements ActionListener {
+
+    static final int screen_width = 600;
+    static final int screen_heigth = 600;
+    static final int unit_size = 25;
+     
+    GamePanel() {
+
+    }
+
+    public void startGame() {
+
+    }
+
+    public void paintComponent(Graphics graphics) {
+
+    }
+
+    public void draw(Graphics graphics) {
+
+    }
+
+    public void move() {
+
+    }
+
+    public void checkApple() {
+
+    }
+
+    public void checkCollisions() {
+
+    }
+
+    public void gameOver(Graphics graphics) {
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+    public class MyKeyAdapter extends KeyAdapter {
+        @Override
+        public void keyPressed(KeyEvent event) {
+
+        }
+    }
+}
